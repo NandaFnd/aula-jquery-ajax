eed9ea5dc7b1319a7caa9a550a944b680cd88a2c
Fernanda Ferreira <nanda_snd@hotmail.com>
2020-06-22 23:47:23 -0300

    Aula Bootstrap JQuery Ajax

---
+++ js/main.js
@@ -0,0 +1,33 @@
+//alert("funcionou");
+
+function consultacep(){
+    $(".barra-progresso").hide();
+    var cep = document.getElementById("cep").value;
+    var url = "https://viacep.com.br/ws/" + cep + "/json/";
+    
+    console.log(url);
+    $.ajax({
+        url: url,
+         type: "GET",
+         success: function(response){
+          console.log(response);
+          $("#logradouro").html(response.logradouro);
+          $("#uf").html(response.uf);
+          $("#localidade").html(response.localidade);
+          $("#bairro").html(response.bairro)
+          $("#titulo_cep").html("CEP" + response.cep);
+          $(".cep").show();
+          $(".barra-progresso").hide();
+         // $("#logradouro").html(response.logradouro);
+         //document.getElementById("logradouro").innerHTML = response.logradouro;
+          //alert(response.logradouro);
+        }
+    })
+
+}
+
+$(function(){
+    $(".cep").hide();
+    $(".barra-progresso").hide();
+})
+
